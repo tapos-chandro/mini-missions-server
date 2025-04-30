410fb650b205f2cd2a325245dbfb68bf3e5c4bbc
tapos-chandro <taposchandro231@gmail.com>
2025-04-30 20:04:03 +0600

    The user data post related api create has been done

--- index.js
+++ index.js
@@ -1,42 +1,74 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 
+app.use(cors())
+app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elvxgab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
-const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.elvxgab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
+const usersCollection = client.db('mini-missions').collection('users')
+
+app.post('/app/v1/users', async (req, res) => {
+    try {
+        const userData = req.body;
+        const filter = { email: userData.email }
+        const findEmail = await usersCollection.findOne(filter)
+
+        if (findEmail) {
+            res.send('already user ')
+            return
+        } else {
+
+            console.log(userData.role)
+            let userAddCoin = {}
+
+            if(userData.role === "buyer"){
+                userAddCoin = {...userData, coins : 50}
+            }else if (userData.role === 'worker'){
+                userAddCoin = {...userData, coins : 10}
+            } else{
+                return
+            }
+
+            const result = await usersCollection.insertOne(userAddCoin)
+            res.send(result)
+
+            
+            
+        }
 
 
+    } catch (error) {
+        console.log(error.message)
+
+    }
+})
 
 
 
